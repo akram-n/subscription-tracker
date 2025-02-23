a5be8adb9e6f4c4461d83a9af15773de882a471c
Noman Akram <akramn.devtools@gmail.com>
2025-02-23 02:53:36 -0500

    add Upstash workflow integration for subscription reminders

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,4 +1,6 @@
 import Subscription from "../models/subscription.model.js";
+import { workflowClient } from "../config/upstash.js";
+import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async (req, res, next) => {
   try {
@@ -7,14 +9,28 @@ export const createSubscription = async (req, res, next) => {
       user: req.user._id,
     });
 
-    res.status(201).json({ success: true, data: subscription });
-  } catch (error) {
-    next(error);
+    const { workflowRunId } = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription.id,
+      },
+      headers: {
+        "content-type": "application/json",
+      },
+      retries: 0,
+    });
+
+    res
+      .status(201)
+      .json({ success: true, data: { subscription, workflowRunId } });
+  } catch (e) {
+    next(e);
   }
 };
 
 export const getUserSubscription = async (req, res, next) => {
   try {
+    // Check if the user is the same as the one in the token
     if (req.user.id !== req.params.id) {
       const error = new Error("You are not the owner of this account");
       error.status = 401;
@@ -24,7 +40,7 @@ export const getUserSubscription = async (req, res, next) => {
     const subscriptions = await Subscription.find({ user: req.params.id });
 
     res.status(200).json({ success: true, data: subscriptions });
-  } catch (error) {
-    next(error);
+  } catch (e) {
+    next(e);
   }
 };
