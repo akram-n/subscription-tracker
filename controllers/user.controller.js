fc1df2f4dfec0c8dacc4d6432694b40295a76d56
Noman Akram <akramn.devtools@gmail.com>
2025-02-24 01:41:37 -0500

    add user management functionality with create, update, and delete operations

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -25,3 +25,50 @@ export const getUser = async (req, res, next) => {
     next(error);
   }
 };
+
+//
+
+export const createUser = async (req, res, next) => {
+  try {
+    const user = await User.create(req.body);
+
+    res.status(201).json({ success: true, data: user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({ success: true, data: user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({ success: true, data: {} });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,13 @@
 import { Router } from "express";
 
 import authorize from "../middlewares/auth.middleware.js";
-import { getUser, getUsers } from "../controllers/user.controller.js";
+import {
+  createUser,
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
@@ -9,16 +15,10 @@ userRouter.get("/", getUsers);
 
 userRouter.get("/:id", authorize, getUser);
 
-userRouter.post("/", (req, res) => {
-  res.send({ title: "CREATE new user" });
-});
+userRouter.post("/", createUser);
 
-userRouter.put("/:id", (req, res) => {
-  res.send({ title: "UPDATE user" });
-});
+userRouter.put("/:id", authorize, updateUser);
 
-userRouter.delete("/:id", (req, res) => {
-  res.send({ title: "DELETE user" });
-});
+userRouter.delete("/:id", authorize, deleteUser);
 
 export default userRouter;
