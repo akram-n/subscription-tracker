5273045febb68fa23ae48447f9066e7c1ab975f8
Noman Akram <akramn.devtools@gmail.com>
2025-02-24 03:07:09 -0500

    refactor nodemailer configuration to use a dedicated accountEmail constant

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,10 +2,12 @@ import nodemailer from "nodemailer";
 
 import { EMAIL, EMAIL_PASSWORD, SERVICE } from "./env.js";
 
+export const accountEmail = EMAIL;
+
 const transporter = nodemailer.createTransport({
   service: SERVICE,
   auth: {
-    user: EMAIL,
+    user: accountEmail,
     pass: EMAIL_PASSWORD,
   },
 });
